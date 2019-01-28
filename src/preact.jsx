9096e0fbc759e7b97530f2b42a3a1f8447dfabb0
FND <fnd@localhost.localdomain>
2019-01-28 15:36:08 +0100

    Add Preact component
    this is pretty much a clone of the React equivalent
    
    caveats:
    
    * fragments don't appear to be supported yet, so using a `span` instead
    * we're not actually defining a `Component`, instead we're just using
      Preact to render JSX (i.e. `h` and `render`) - which might distort
      results

---
+++ src/preact.jsx
@@ -0,0 +1,13 @@
+import { h, render } from 'preact';
+
+class PreactEmpty extends HTMLElement {
+  connectedCallback() {
+    const mountPoint = document.createElement('span');
+    this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+    let text = this.getAttribute('text');
+
+    render(<span><style>:host  {'{'} border: 1px solid red; padding: 0.5em; {'}'} </style>{text}</span>, mountPoint);
+  }
+}
+
+customElements.define('preact-empty', PreactEmpty);
