f839f4a61db3e353adc4584e916b67bc6d900729
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-11-25 15:18:59 +0100

    Add svelte example

---
+++ cli_based_builds/svelte/src/App.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="svelte-empty" />
+
+<script>
+  export let text;
+</script>
+
+<style>
+	:host {
+    border: 1px solid red;
+    padding: 0.5em;
+  }
+</style>
+
+<span>{text}</span>
