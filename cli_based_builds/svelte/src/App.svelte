c2ecf1ce6adbff1233b5d1fb149ca768d85202ea
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2026-04-30 11:10:17 +0200

    Add "ping" feature for svelte

--- cli_based_builds/svelte/src/App.svelte
+++ cli_based_builds/svelte/src/App.svelte
@@ -3,6 +3,19 @@
 <script>
   import { VERSION } from 'svelte/src/version.js';
   let { text } = $props();
+  let lastPing = $state(null);
+
+  function ping() {
+    window.dispatchEvent(new CustomEvent('framework-ping', {
+      detail: 'svelte-empty', bubbles: true, composed: true
+    }));
+  }
+
+  $effect(() => {
+    function onPing(e) { lastPing = e.detail; }
+    window.addEventListener('framework-ping', onPing);
+    return () => window.removeEventListener('framework-ping', onPing);
+  });
 </script>
 
 <style>
@@ -10,7 +23,13 @@
     display: inline-block;
     border: 1px solid red;
     padding: 0.5em;
+    min-width: 20em;
   }
 </style>
 
-<span>{text}<br><small>Svelte {VERSION}</small></span>
+<span>
+  {text}<br>
+  <button onclick={ping}>Ping</button>
+  {#if lastPing}<span> ← {lastPing}</span>{/if}<br>
+  <small style="text-align: right; display: block;">Svelte {VERSION}</small>
+</span>
